6fd13875eddae1ab11af19d4ce15448dc0994f4f
gabe <gabriel808@gmail.com>
2019-08-07 15:08:37 -0700

    added test file

---
+++ test/test.py
@@ -0,0 +1,49 @@
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+from DeepRL.deep_rl import *
+
+
+def ppo_continuous(**kwargs):
+    generate_tag(kwargs)
+    kwargs.setdefault('log_level', 0)
+    config = Config()
+    config.merge(kwargs)
+
+    config.task_fn = lambda: Task(config.game)
+    config.eval_env = config.task_fn()
+
+    config.network_fn = lambda: GaussianActorCriticNet(
+        config.state_dim, config.action_dim, actor_body=FCBody(config.state_dim, gate=torch.tanh),
+        critic_body=FCBody(config.state_dim, gate=torch.tanh))
+    config.optimizer_fn = lambda params: torch.optim.Adam(params, 3e-4, eps=1e-5)
+    config.discount = 0.99
+    config.use_gae = True
+    config.gae_tau = 0.95
+    config.gradient_clip = 0.5
+    config.rollout_length = 2048
+    config.optimization_epochs = 10
+    config.mini_batch_size = 64
+    config.ppo_ratio_clip = 0.2
+    config.log_interval = 2048
+    config.max_steps = 1e6
+    config.state_normalizer = MeanStdNormalizer()
+    run_steps(PPOAgent(config))
+
+
+if __name__ == '__main__':
+    mkdir('log')
+    mkdir('tf_log')
+    set_one_thread()
+    random_seed()
+    select_device(-1)
+    # select_device(0)
+
+    env = 'alphred-v3'
+    from envs.gazebo_envs import register_alphredv3
+    register_alphredv3()
+    ppo_continuous(game=env)
+
