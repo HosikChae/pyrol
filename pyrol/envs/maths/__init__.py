7f83982db3749db93259cb2c41f82338bce49399
gabe <gabriel808@gmail.com>
2019-08-13 07:45:09 -0700

    added more files

---
+++ pyrol/envs/maths/__init__.py
@@ -0,0 +1,15 @@
+from gym.envs.registration import register
+
+def register_all_envs():
+
+    register(
+        id='Pendulum-maths0',
+        entry_point='maths.pendulum:PendulumEnv',
+        max_episode_steps=1000,
+        reward_threshold=6000.0,
+    )
+
+    # Add other maths environments here
+
+    return None
+
