8f0d7eb3e6e365b606b2126744dd4ce6a0534613
gabe <gabriel808@gmail.com>
2019-08-13 07:53:20 -0700

    Deleted extraneous files

--- envs/gazebo_envs/alphredv3/Simulation/Gazebo/__init__.py
+++
@@ -1,8 +0,0 @@
-#!usr/bin/env python
-__author__ = "Colin Togashi"
-__email__ = "colin.togashi@gmail.com"
-__copyright__ = "Copyright 2019 RoMeLa"
-__date__ = "May 7, 2019"
-
-__version__ = "0.0.1"
-__status__ = "Prototype"
